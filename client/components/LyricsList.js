0d12ff1d7def3f7da4a69831607bcd97f69a8d3b
halim <haleemyoosuph@gmail.com>
2020-12-01 03:01:51 +0100

    feat: can like lyric

--- client/components/LyricsList.js
+++ client/components/LyricsList.js
@@ -1,21 +1,55 @@
 import React from 'react';
+import { graphql } from 'react-apollo';
 
-const LyricList = ({ lyrics }) => {
+import likeLyric from '../queries/LikeLyric';
+
+const LyricList = (props) => {
+  const { lyrics, mutate } = props;
+  const onLike = (id, likes) => {
+    mutate({
+      variables: {
+        id,
+      },
+      optimisticResponse: {
+        __typename: 'Mutation',
+        likeLyric: {
+          id,
+          likes: likes + 1,
+          __typename: 'LyricType',
+        },
+      },
+    }).then(() => {
+      console.log('liked');
+    });
+  };
+
   return (
-    <div className='pt-10'>
-      <ul className='list-group'>
-        {lyrics.map((a) => (
-          <li
-            className='list-group-item d-flex justify-content-between align-items-center'
-            key={a.id}
-          >
-            <div>{a.content}</div>
-            <button className='btn btn-primary btn-sm'>Like</button>
-          </li>
-        ))}
-      </ul>
+    <div>
+      <div className='my-3'>
+        <ul className='list-group'>
+          {lyrics.map((a) => (
+            <li
+              className='list-group-item d-flex justify-content-between align-items-center'
+              key={a.id}
+            >
+              <div>{a.content}</div>
+              <div>
+                <span className='badge badge-primary badge-pill mx-3'>
+                  {a.likes}
+                </span>
+                <button
+                  onClick={() => onLike(a.id, a.likes)}
+                  className='btn btn-primary btn-sm'
+                >
+                  Like
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
 
-export default LyricList;
+export default graphql(likeLyric)(LyricList);
